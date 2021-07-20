485d25dfb9fb120ac2c40fff9164434128f3e935
Pablo Saavedra G <pablosaa@uni-bonn.de>
2021-07-20 22:33:20 +0200

    including Cloudnet file to match with spectrum

--- convert_kazr_spec2zarr.jl
+++ convert_kazr_spec2zarr.jl
@@ -1,14 +1,15 @@
 # script to convert the KAZR spectrum to Zarr file compatible with Voodoo
-using Zarr
 
 # Including ARM toolbox to read data:
 include(joinpath(homedir(), "LIM/repos/ARMtools.jl/src/ARMtools.jl"))
+include(joinpath(homedir(), "LIM/repos/CloudnetTools.jl/src/CloudnetTools.jl"))
 
 # Defining constants:
 ARM_SITE = "utqiagvik-nsa"
 PATH = joinpath(homedir(), "LIM/remsens", ARM_SITE)
 RADARPRO = "KAZR/ARSCL"
 SPECTPRO = "KAZR/SPECCOPOL"
+CLNET = joinpath(homedir(), "LIM/data/CloudNet/output")
 # Date and hour to be read (spectrum data comes in hourly data files)
 
 yy = 2019;
@@ -16,62 +17,27 @@ mm = 1;
 dd = 27;
 hh = 03;
 
+# reading CloudNet categorize data file:
+clnfile = ARMtools.getFilePattern(CLNET, "CEIL10m", yy, mm, dd; submonth=true)
+cln = CloudNet.readCLNFile(clnfile);
+
 # reading Radar Data
-rafile = ARMtools.getFilePattern(PATH, RADARPRO, yy, mm, dd)
-kazr = ARMtools.getKAZRData(rafile);
+#rafile = ARMtools.getFilePattern(PATH, RADARPRO, yy, mm, dd)
+#kazr = ARMtools.getKAZRData(rafile);
 
 # reading Spectrum Data
-spfile = ARMtools.getFilePattern(PATH, SPECTPRO, yy, mm, dd, hh)
+spfile = ARMtools.getFilePattern(PATH, SPECTPRO, yy, mm, dd; hh)
 #spfile = "/home/pablo/LIM/data/utqiagvik-nsa/KAZR/SPECCOPOL/nsakazrspeccmaskgecopolC1.a0.20190127.010010.nc";
 spec = ARMtools.readSPECCOPOL(spfile);
 nt, nh = size(spec[:spect_mask]);
 
-p = "data/kazrspec.zarr";
-
-Z = zcreate(eltype(spec[:spect_mask]), nt, nh, path=p, name="spec_mask");
-
-Z[:,:] = spec[:spect_mask]);
-
-# the above doesn't work!!
+## MATCHING the CloudNet time vector with the Spectrum time vector:
+ii = #select indexes in cln[:time] corresponding to spec[:time];
 
 ## TRYING with PyCall
 using PyCall, Dates
 xr = pyimport("xarray")
-
-temperature = 15 .+ 8 .* randn(2, 2, 3)
-precipitation = 10 .* rand(2, 2, 3)
-lon = [-99.83 -99.32; -99.79 -99.23]
-lat = [42.25 42.21; 42.63 42.59]
-thetime = DateTime(2014,09,06):Hour(1):DateTime(2014,09,06,2,0,0); #pd.date_range("2014-09-06", periods=3)
-reference_time = DateTime(2014,09,05)
-
-ds = xr.Dataset(
-data_vars=Dict(
-        :temperature=>(["x", "y", "time"], temperature),
-        :precipitation=>(["x", "y", "time"], precipitation),
-),
-coords=Dict(
-        :lon=>(["x", "y"], lon),
-        :lat=>(["x", "y"], lat),
-        :time=>thetime,
-        :reference_time=>reference_time,
-),
-attrs=Dict(:description=>"Weather related data."),
-)
-
-ds = xr.Dataset(
-data_vars=Dict(
-        :temperature=>  temperature,
-        :precipitation=>precipitation,
-),
-coords=Dict(
-        :lon=>lon,
-        :lat=>lat,
-        :time=>thetime,
-),
-attrs=Dict(:description=>"Weather related data."),
-)
-
+da = pyimport("dask.array")
 
 ## *** To fill data with KAZR SPEC ****
 # For coordinates:
@@ -116,7 +82,52 @@ VariableND = xr.Dataset(
     attrs = my_att,
 );
 
+# ØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØ
+# the following doesn't work!!
 # Storing as Zarr
 VariablesND.to_zarr(store=filename_zarr)
+
+p = "data/kazrspec.zarr";
+
+Z = zcreate(eltype(spec[:spect_mask]), nt, nh, path=p, name="spec_mask");
+
+Z[:,:] = spec[:spect_mask]);
+# ØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØ
+# the following just as example:
+temperature = 15 .+ 8 .* randn(2, 2, 3)
+precipitation = 10 .* rand(2, 2, 3)
+lon = [-99.83 -99.32; -99.79 -99.23]
+lat = [42.25 42.21; 42.63 42.59]
+thetime = DateTime(2014,09,06):Hour(1):DateTime(2014,09,06,2,0,0);
+reference_time = DateTime(2014,09,05)
+
+ds = xr.Dataset(
+data_vars=Dict(
+        :temperature=>(["x", "y", "time"], temperature),
+        :precipitation=>(["x", "y", "time"], precipitation),
+),
+coords=Dict(
+        :lon=>(["x", "y"], lon),
+        :lat=>(["x", "y"], lat),
+        :time=>thetime,
+        :reference_time=>reference_time,
+),
+attrs=Dict(:description=>"Weather related data."),
+)
+
+ds = xr.Dataset(
+data_vars=Dict(
+        :temperature=>  temperature,
+        :precipitation=>precipitation,
+),
+coords=Dict(
+        :lon=>lon,
+        :lat=>lat,
+        :time=>thetime,
+),
+attrs=Dict(:description=>"Weather related data."),
+)
+# ØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØ
+
 
 # end of script
