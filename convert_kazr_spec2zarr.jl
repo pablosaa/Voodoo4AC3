035e7ce087801d5f04911cdee67a670b6683df87
Pablo Saavedra G <pablosaa@uni-bonn.de>
2021-07-26 22:46:59 +0200

    dimensions fixed and spectrum normilized to 0-1

--- convert_kazr_spec2zarr.jl
+++ convert_kazr_spec2zarr.jl
@@ -68,7 +68,11 @@ n_samples = sum(masked);
 n_velocity = length(spec[:vel_nn]);
 n_channels = 6;
 n_pol = 2;
-features = Array{Float32}(undef, n_samples, n_velocity, n_channels, n_pol) .= NaN32;
+
+# Creating features tensor:
+#features = Array{Float32}(undef, n_samples, n_velocity, n_channels, n_pol) .= NaN32;
+features = Array{Float32}(undef, n_pol, n_channels, n_velocity, n_samples) .= NaN32;
+η_hh = CloudNet.TransformZeroOne(spec[:η_hh]);
 
 i_sample = 0;
 for i_rg ∈ (1:n_rg)
@@ -77,12 +81,13 @@ for i_rg ∈ (1:n_rg)
             global i_sample += 1
             idx_alt = 1 .+ filter(x->x ≥ 0, spec[:spect_mask][3+i_rg, ii_6spect[i_ts]])
             n_alt = length(idx_alt)
-            features[i_sample, :, 1:n_alt, 1] = spec[:η_hh][:, idx_alt];
+            #features[i_sample, :, 1:n_alt, 1] = spec[:η_hh][:, idx_alt];
+            features[1, 1:n_alt, :, i_sample] = transpose(η_hh[:, idx_alt]);
         end
     end
 end
 
-# Estimating time variables:
+# Estimating time variables [milliseconds]:
 dt = Dates.value.(ii .- ii[1]);
 
 ## TRYING with PyCall
@@ -93,12 +98,14 @@ da = pyimport("dask.array")
 ## Converting Julia variables to dask:
 ## ***** For N-D variales ***********
 FeaturesArray = da.from_array(features,
-                              chunks = (floor(n_samples/4),
-                                        floor(n_velocity/4),
-                                        2, 1));
-MaskedArray = da.from_array(masked, chunks=(n_rg, n_ts));
-MultiTargets = da.from_array(Array{Float32}(undef, n_samples, n_channels),
-                             chunks = (floor(n_samples/2), n_channels))
+                              chunks = (1, 2, floor(n_velocity/4),
+                                        floor(n_samples/4)) );
+                                        
+MaskedArray = da.from_array(transpose(masked), chunks=(n_ts, n_rg));
+
+MultiTargets = da.from_array(Array{Float32}(undef, n_channels, n_samples),
+                             chunks = (n_channels, floor(n_samples/2) ))
+
 TargetsVec = da.from_array(Array{Int32}(undef, n_samples), chunks=(n_samples));
 
 ## *** To fill data with KAZR SPEC ****
@@ -110,7 +117,7 @@ TargetsVec = da.from_array(Array{Int32}(undef, n_samples), chunks=(n_samples));
 #  (nsamples) int64 0 1 2 3 4 5 ... 11855 11856 11857 11858 11859
 #  (nvelocity) int64 0 1 2 3 4 5 6 ... 250 251 252 253 254 255
 #    (rg) float32 119.247086 149.05884 ... 11924.608 11964.357
-my_coor = Dict(
+infoND_coor = Dict(
     :dt => dt,
     :nbits => collect(Int64, 0:5),
     :nchannels => collect(Int64, 0:5),
@@ -125,15 +132,15 @@ my_coor = Dict(
 # (nsamples, nvelocity, nchannels, npol) float32 dask.array<shape=(11860, 256, 6, 2), chunksize=(2965, 64, 2, 1)>
 # bool dask.array<shape=(120, 292), chunksize=(120, 292)>
 # float32 dask.array<shape=(11860, 6), chunksize=(5930, 6)>
-my_vars = Dict(
-    :features => ([:nsamples, :nvelocity, :nchannels, :npol], FeaturesArray),
-    :masked => ([:rg, :ts], MaskedArray),
-    :multitargets => ([:nsamples, :nbits], MultiTargets),
+infoND_vars = Dict(
+    :features => ([:npol, :nchannels, :nvelocity, :nsamples], FeaturesArray),
+    :masked => ([:ts, :rg], MaskedArray),
+    :multitargets => ([:nbits, :nsamples], MultiTargets),
     :targets => (:nsamples, TargetsVec),
 );
 
 # For Attributes:
-my_att = Dict(
+infoND_att = Dict(
     :dt_unit       => "date",
     :dt_unit_long  => "Datetime format",
     :nbits_unit    => "Number of Cloudnet category bits",
@@ -149,9 +156,9 @@ my_att = Dict(
 
 # Creating N-D XARRAY Dataset:
 VariableND = xr.Dataset(
-    data_vars = my_vars,
-    coords = my_coor,
-    attrs = my_att,
+    data_vars = infoND_vars,
+    coords = infoND_coor,
+    attrs = infoND_att,
 );
 
 
@@ -174,10 +181,42 @@ info2D_coor = Dict(
 # For 2-D dask variables:
 info2D_vars = Dict(
     # Int32
-    :CLASS => ([:rg, :ts], da.from_array(cln[:CLASSIFY][1:n_rg, idx_ts],
-                                         chunks = (n_rg, n_ts) )),
+    :CLASS => ([:ts, :rg], da.from_array(transpose(cln[:CLASSIFY][1:n_rg, idx_ts]),
+                                         chunks = (n_ts, n_rg) )),
 
     :LWP => (:ts, da.from_array(cln[:LWP][idx_ts], chunks = (n_ts) )),
+
+    :VEL  => ([:ts, :rg], da.from_array(transpose(cln[:V][1:n_rg, idx_ts]),
+                                        chunks = (n_ts, n_rg) )),
+
+    :VEL_sigma => ([:ts, :rg], da.from_array(transpose(cln[:σV][1:n_rg, idx_ts]),
+                                             chunks = (n_ts, n_rg) )),
+
+    :Z     => ([:ts, :rg], da.from_array(transpose(cln[:Z][1:n_rg, idx_ts]),
+                                         chunks=(n_ts, n_rg) )),
+                                     
+    :beta  => ([:ts, :rg], da.from_array(transpose(cln[:β][1:n_rg, idx_ts]),
+                                         chunks = (n_ts, n_rg) )),
+
+    :category_bits => ([:ts, :rg],
+                       da.from_array(transpose(cln[:CATEBITS][1:n_rg, idx_ts]),
+                                     chunks = (n_ts, n_rg) )),
+    # Int32
+    :detection_status => ([:ts, :rg],
+                          da.from_array(transpose(cln[:DETECTST][1:n_rg, idx_ts]),
+                                        chunks = (n_ts, n_rg) )),
+    # Float32
+    :insect_prob => ([:ts, :rg],
+                     da.from_array(transpose(cln[:P_INSECT][1:n_rg, idx_ts]),
+                                   chunks = (n_ts, n_rg) )),
+    
+    :quality_bits => ([:ts, :rg],
+                      da.from_array(transpose(cln[:QUALBITS][1:n_rg, idx_ts]),
+                                    chunks = (n_ts, n_rg) )),
+    
+    :width => ([:ts, :rg], da.from_array(transpose(cln[:ωV][1:n_rg, idx_ts]),
+                                         chunks = (n_ts, n_rg) )),
+);
 
     #:P => ([:rg, :ts], da.from_array(cln[:Pa][1:n_rg, idx_ts],
     #                                 chunks = (n_rg, floor(n_ts/2)) )),
@@ -190,40 +229,12 @@ info2D_vars = Dict(
 
     #:UWIND=> ([:rg, :ts], da.from_array(cln[:UWIND][1:n_rg, idx_ts],
     #                                chunks = (n_rg, floor(n_ts/2)) )),
-
-    :VEL  => ([:rg, :ts], da.from_array(cln[:V][1:n_rg, idx_ts],
-                                        chunks = (n_rg, n_ts) )),
-
-    :VEL_sigma => ([:rg, :ts], da.from_array(cln[:σV][1:n_rg, idx_ts],
-                                             chunks = (n_rg, n_ts) )),
-
     #:VWIND => ([:rg, :ts], da.from_array(cln[:VWIND][1:n_rg, idx_ts],
     #                                     chunks=(n_rg, floor(n_ts/2)) )),
-
-    :Z     => ([:rg, :ts], da.from_array(cln[:Z][1:n_rg, idx_ts],
-                                         chunks=(n_rg, n_ts) )),
-                                     
-    :beta  => ([:rg, :ts], da.from_array(cln[:β][1:n_rg, idx_ts],
-                                         chunks = (n_rg, n_ts) )),
-
-    :category_bits => ([:rg, :ts], da.from_array(cln[:CATEBITS][1:n_rg, idx_ts],
-                                                 chunks = (n_rg, n_ts) )),
-    # Int32
-    :detection_status => ([:rg, :ts], da.from_array(cln[:DETECTST][1:n_rg, idx_ts],
-                                                    chunks = (n_rg, n_ts) )),
-    # Float32
-    :insect_prob => ([:rg, :ts], da.from_array(cln[:P_INSECT][1:n_rg, idx_ts],
-                                               chunks = (n_rg, n_ts) )),
-    
     #:q => ([:rg, :ts], da.from_array(cln[:Qv][1:n_rg, idx_ts],
     #                                 chunks = (n_rg, floor(n_ts/2)) )),
     
-    :quality_bits => ([:rg, :ts], da.from_array(cln[:QUBITS][1:n_rg, idx_ts],
-                                                chunks = (n_rg, n_ts) )),
-    
-    :width => ([:rg, :ts], da.from_array(cln[:ωV][1:n_rg, idx_ts],
-                                         chunks = (n_rg, n_ts) )),
-);
+
 
 # For attributes 2-D variables
 info2D_attr = Dict(
@@ -244,6 +255,8 @@ Variable2D = xr.Dataset(
 
 Variable2D.to_zarr(store = replace(filename_zarr, "ND" => "2D" ))
 
+
+## END IF SCRIPT
 
 # ØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØØ
 # the following doesn't work!!
